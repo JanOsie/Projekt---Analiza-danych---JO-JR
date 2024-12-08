c4ff4f324cd3d905085688c9112ad38927e62e3d
Jan Osiecki <s186908@student.pg.edu.pl>
2024-12-08 13:02:43 +0100

    dodałem kod do pobierania pakietówi przesortowałem wstępnie

--- Kody/czyszczenie.R
+++ Kody/czyszczenie.R
@@ -2,3 +2,15 @@
 
 dane <- read.csv("dane_surowe/samochody_new.csv")
 View(dane)
+
+#instalacja pakietów
+install.packages("tidyverse")
+
+library(tidyverse)
+
+dane <- dane %>% 
+  arrange(dane, brand, model)
+
+View(dane)
+
+
