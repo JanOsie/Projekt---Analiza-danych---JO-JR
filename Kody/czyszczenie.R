004f9a954b0f765d8e2a89d56d13d9d633c255ac
Julia <186922ziv@gmail.com>
2024-12-29 16:01:30 +0100

    usunęłam sprawdzanie NA oraz wyczyściłam kolumny: paliwo i rok

--- Kody/czyszczenie.R
+++ Kody/czyszczenie.R
@@ -25,72 +25,320 @@ is_tibble(dane)
 
 colnames(dane) <- c("marka", "model", "cena_zl", "przebieg_w_milach", "skrzynia_biegow", "poj_silnika", "paliwo", "miasto", "wojewodztwo", "rok" )
 
-# ile NA w całej tabeli(5100), proporcja (0,00557 albo 0,5%)
+# zapisanie prawidłowych wartości w kolumnie paliwo
 
-n_miss(dane)
-prop_miss(dane)
+paliwo_prawidlowe <- c("Benzyna", "Benzyna+LPG", "Diesel", "Hybryda", "Elektryczny", "Benzyna+CNG", "Wodór")
 
-# ile danych dla jakiej kolumny i grupy
+# przefiltrowanie danych, które nie zwierają odpowiednich wartości w kolumnie "paliwo"
 
-dane %>%
-  group_by(marka) %>%
-  miss_var_summary()
+paliwo_blednie <- dane %>% 
+  filter(!(paliwo %in% paliwo_prawidlowe))
 
-# liczba braków dla konkretnej ilości braków w wierszu
+# istnieje 5,503 błędnych obserwacji
 
-dane %>%
-  miss_case_table()
+View(paliwo_blednie)
 
-# wizualizacja braków
-# najwięcej braków mamy dla ferrari (5%) i maserati (5%)
+# ile z tych obserwacji ma wpisany przebieg (zawierających tekst "km") w kolumnie paliwo
 
-gg_miss_fct(dane, fct = marka)
+paliwo_blednie_km <- filter(paliwo_blednie, grepl("km", paliwo))
 
-# zbadanie współwystępowania braków
-# braki danych występują prawie wyłącznie pojedyczo
-gg_miss_upset(dane, nsets = 10)
+# 4850 pozycji ma przebieg w miejscu paliwa
 
-# szukanie zależności między zmiennymi
+View(paliwo_blednie_km)
 
-dane_ferrari <- dane %>%
-  filter(marka == "ferrari")
+########################## TEST NA PODZBIORZE ##########################
 
-#braki danych są rozrzucone dla ferrari i nie wykazują jakiejś zależności
+# tworzenie pętli by zamienić paliwo z przebiegiem
 
-ggplot(data = dane_ferrari, aes(x = rok, y = cena_zl)) +
-  geom_point() +
-  geom_miss_point() +
-  scale_color_manual(values = c("blue","red")) +
-  theme_minimal()
+dane2 <- head(paliwo_blednie_km)
+view(dane2)
 
-# sprawdzenie brakujących danych dla wszytskich modeli (za dużo, trzba ograniczyć model)
+# dla każego wiersza w dane2
+for (i in 1:nrow(dane2)) {
+  # jeśli w kolumnie przebieg_w_milach wartość zawiera coś z paliwo_prawidlowe
+  if (dane2$przebieg_w_milach[i] %in% paliwo_prawidlowe) {
+    # Zamiana miejscami
+    paliwo_km <- dane2$paliwo[i]
+    dane2$paliwo[i] <- dane2$przebieg_w_milach[i]
+    dane2$przebieg_w_milach[i] <- paliwo_km
+  }
+}
 
-ggplot(data = dane, aes(x = rok, y = cena_zl)) +
-  geom_point(aes(color = marka, shape = marka), size = 2) +
-  theme_minimal()
+View(dane2)
 
-# sprawdzam czy dane są wymieszane 
+###################### OCZYSZCZANIE W CAŁEJ TABELI ######################
 
-unique(dane$skrzynia_biegow)
-unique(dane$paliwo)
+# zadziałało na przykładzie :D, teraz z użyciem filtru
 
-# ile obserwacji mają złą wartość w kolumnie paliwo (piwo to Janka paliwo)
+dane4 <- dane
 
-spr <- !grepl("km",dane$przebieg_w_milach)
-sum(spr)
+for (i in 1:nrow(dane4)) {
+  if (grepl("km", dane4$paliwo[i])) {
+    if (dane4$przebieg_w_milach[i] %in% paliwo_prawidlowe) {
+      paliwo_z_km <- dane4$paliwo[i]
+      dane4$paliwo[i] <- dane4$przebieg_w_milach[i]
+      dane4$przebieg_w_milach[i] <- paliwo_z_km
+    }
+  }
+}
 
-# usuwam km i spację w kolumnie przebieg w milach 
+View(dane4)
 
-dane$przebieg_w_milach2 <- gsub('km', '', dane$przebieg_w_milach)
+#sprawdzenie co zostało 
 
-dane$przebieg_w_milach2 <- trimws(dane$przebieg_w_milach)
+paliwo_blednie4 <- dane4 %>% 
+  filter(!(paliwo %in% paliwo_prawidlowe))
 
-dane$przebieg_w_milach2 <- gsub(' ', '', dane$przebieg_w_milach)
+unique(paliwo_blednie4$paliwo)
+View(paliwo_blednie4)
 
-dane$przebieg_w_milach2 <- gsub('km', '', dane$przebieg_w_milach2)
+# zostało 653 błędnych obserwacji czyli rok i pojemność
 
-dane$przebieg_w_milach2 <- as.numeric(dane$przebieg_w_milach2)
+###################### OCZYSZCZANIE W CAŁEJ TABELI ######################
 
-n_miss(dane$przebieg_w_milach)
+# pętla do poprawy cm3
 
-n_miss(dane$przebieg_w_milach2)
+dane5 <- dane4
+
+for (i in 1:nrow(dane5)) {
+  if (grepl("cm3", dane5$paliwo[i])) {
+    if (dane5$przebieg_w_milach[i] %in% paliwo_prawidlowe) {
+      paliwo_z_km <- dane5$paliwo[i]
+      dane5$paliwo[i] <- dane5$przebieg_w_milach[i]
+      dane5$przebieg_w_milach[i] <- paliwo_z_km
+    }
+  }
+}
+
+View(dane5)
+
+# sprawdzenie co zostało (563 obs)
+
+paliwo_blednie5 <- dane5 %>% 
+  filter(!(paliwo %in% paliwo_prawidlowe))
+
+unique(paliwo_blednie5$paliwo)
+View(paliwo_blednie5)
+
+# jakie wiersze zawierają pojemność silnika w kolumnie przebieg_w_milach (221 obs)
+
+cm3_w_przebiegu <- dane5[grepl("cm3", dane5$przebieg_w_milach), ]
+View(cm3_w_przebiegu)
+# gdy zmienna rok jest w poj silnika albo paliwo, to w kolumnie rok została wpisana pojemność silnika
+
+# najpierw poprawiam kolumnę paliwo
+
+########################## TEST NA PODZBIORZE ##########################
+
+cm3_w_przebiegu2 <- cm3_w_przebiegu
+
+for (i in 1:nrow(cm3_w_przebiegu2)) {
+  if (cm3_w_przebiegu2$poj_silnika[i] %in% paliwo_prawidlowe) {
+    paliwo_z_poj_silnika <- cm3_w_przebiegu2$poj_silnika[i]
+    cm3_w_przebiegu2$poj_silnika[i] <- cm3_w_przebiegu2$paliwo[i]
+    cm3_w_przebiegu2$paliwo[i] <- paliwo_z_poj_silnika
+  }
+}
+
+View(cm3_w_przebiegu2)
+
+###################### OCZYSZCZANIE W CAŁEJ TABELI ######################
+
+# zadziałała zamiana więc na głównej tabeli zamieniam
+
+dane6 <- dane5
+
+for (i in 1:nrow(dane6)) {
+  if (dane6$poj_silnika[i] %in% paliwo_prawidlowe) {
+    paliwo_z_poj_silnika <- dane6$poj_silnika[i]
+    dane6$poj_silnika[i] <- dane6$paliwo[i]
+    dane6$paliwo[i] <- paliwo_z_poj_silnika
+  }
+}
+
+# sprawdzam czy zadziałało
+
+unique(dane6$paliwo)
+View(paliwo_blednie6)
+glimpse(paliwo_blednie6$paliwo)
+
+
+
+
+########################################################################
+# ZMIENNA PALIWO ZOSTAŁA OCZYSZCZONA
+########################################################################
+
+
+
+
+# sprawdzenie kolumny rok, jakie dane poza rokiem są w kolumnie rok
+
+unique(dane6$rok)
+
+# jest wiele obserwacji z cm3 lub km, sprawdzam czy są one zamienione miejscami
+
+# tworzenie i zapisanie roku jako tekst do późniejszego przefiltrowania
+rok_prawidłowe <- c(1990:2025)
+rok_prawidłowe <- as.character(rok_prawidłowe)
+is.character(rok_prawidłowe)
+glimpse(rok_prawidłowe)
+
+# sprawdzenie ile jest obserwacji innych niż rok w kolumnie rok (7566 obs)
+
+rok_blednie6 <- dane6 %>% 
+  filter(!(rok %in% rok_prawidłowe))
+
+View(rok_blednie6)
+
+# jakie wiersze zawierają pojemność silnika w kolumnie rok
+
+cm3_w_rok <- rok_blednie6[grepl("cm3", rok_blednie6$rok), ]
+View(cm3_w_rok)
+# jest 4,399 obserwacji, w których podano rok w poj_silnika a poj_silnia w rok
+
+########################## TEST NA PODZBIORZE ##########################
+
+# pętla do zamiany roku i poj_silnika - próba na cm3_w_rok
+
+cm3_w_rok2 <- cm3_w_rok
+
+for (i in 1:nrow(cm3_w_rok2)) {
+  if (grepl("cm3", cm3_w_rok2$rok[i])) {
+    if (cm3_w_rok2$poj_silnika[i] %in% rok_prawidłowe) {
+      rok_z_cm3 <- cm3_w_rok2$rok[i]
+      cm3_w_rok2$rok[i] <- cm3_w_rok2$poj_silnika[i]
+      cm3_w_rok2$poj_silnika[i] <- rok_z_cm3
+    }
+  }
+}
+
+View(cm3_w_rok2)
+
+###################### OCZYSZCZANIE W CAŁEJ TABELI ######################
+
+# poprawnie zamieniono, teraz pora na zamiane w najnowszej tabeli "dane6"
+
+dane7 <- dane6
+
+for (i in 1:nrow(dane7)) {
+  if (grepl("cm3", dane7$rok[i])) {
+    if (dane7$poj_silnika[i] %in% rok_prawidłowe) {
+      rok_z_cm3 <- dane7$rok[i]
+      dane7$rok[i] <- dane7$poj_silnika[i]
+      dane7$poj_silnika[i] <- rok_z_cm3
+    }
+  }
+}
+
+unique(dane7$rok)
+
+# teraz zostały do poprawy wartrości z km, sprawdzam czy wartości kolumn przebiegu i roku są na odwrót
+
+# sprawdzenie ile jest obserwacji innych niż rok w kolumnie rok i nie będące NA (717 obs)
+
+rok_blednie7 <- dane7 %>% 
+  filter(!(rok %in% rok_prawidłowe) & !is.na(rok))
+
+View(rok_blednie7)
+
+# wartości z km mają wpisane rok w poj_silnika, przebieg mają też wpisany ten sam albo inny
+# zauważyłem, że gdy wpisano paliwo w kol rok, to rok znajduje się w kol przebieg_w_milach (299 obs)
+
+paliwo_w_rok <- rok_blednie7 %>% 
+  filter(rok %in% paliwo_prawidlowe)
+
+View(paliwo_w_rok)
+
+########################## TEST NA PODZBIORZE ##########################
+
+# przeniesienie z przebieg (w którym jest rok) do kol rok
+
+# pętla do zamiany roku i poj_silnika - próba na cm3_w_rok
+
+paliwo_w_rok2 <- paliwo_w_rok
+
+for (i in 1:nrow(paliwo_w_rok2)) {
+  if (paliwo_w_rok2$rok[i] %in% paliwo_prawidlowe) {
+    if (paliwo_w_rok2$przebieg_w_milach[i] %in% rok_prawidłowe) {
+      # nie ma potrzeby zapisać paliwa z kol rok, bo są to duplikaty (w kol paliwo już jest wartość)
+      paliwo_w_rok2$rok[i] <- paliwo_w_rok2$przebieg_w_milach[i]
+      paliwo_w_rok2$przebieg_w_milach[i] <- NA
+    }
+  }
+}
+
+View(paliwo_w_rok2)
+
+###################### OCZYSZCZANIE W CAŁEJ TABELI ######################
+
+# pętla na całej tabeli teraz
+
+dane8 <- dane7
+
+for (i in 1:nrow(dane8)) {
+  if (dane8$rok[i] %in% paliwo_prawidlowe) {
+    if (dane8$przebieg_w_milach[i] %in% rok_prawidłowe) {
+      # nie ma potrzeby zapisać paliwa z kol rok, bo są to duplikaty (w kol paliwo już jest wartość)
+      dane8$rok[i] <- dane8$przebieg_w_milach[i]
+      dane8$przebieg_w_milach[i] <- NA
+    }
+  }
+}
+
+View(dane8)
+
+# sprawdzam jakie unikatowe wartości zostały w kolumnie rok
+
+unique(dane8$rok)
+
+# został tylko przebieg, który zamieniamy z poj_silnika, a poj_silnika zamieniamy na NA po zmianie
+
+km_w_rok <- dane8 %>% 
+  filter(!(rok %in% rok_prawidłowe) & !is.na(rok))
+
+km_w_rok2 <- km_w_rok[grepl("km", km_w_rok$rok), ]
+
+View(km_w_rok2)
+
+km_w_rok3 <- km_w_rok2
+
+for (i in 1:nrow(km_w_rok3)) {
+  if (grepl("km", km_w_rok3$rok[i])) {
+    if (km_w_rok3$poj_silnika[i] %in% rok_prawidłowe) {
+      km_w_rok3$rok[i] <- km_w_rok3$poj_silnika[i]
+      km_w_rok3$poj_silnika[i] <- NA
+    }
+  }
+}
+
+View(km_w_rok3)
+
+# poprawiono 418 obs
+
+###################### OCZYSZCZANIE W CAŁEJ TABELI ######################
+
+# poprawa na całej tabeli
+
+dane9 <- dane8
+
+for (i in 1:nrow(dane9)) {
+  if (grepl("km", dane9$rok[i])) {
+    if (dane9$poj_silnika[i] %in% rok_prawidłowe) {
+      dane9$rok[i] <- dane9$poj_silnika[i]
+      dane9$poj_silnika[i] <- NA
+    }
+  }
+}
+
+View(dane9)
+unique(dane9$rok)
+
+
+
+
+
+########################################################################
+# ZMIENNA ROK ZOSTAŁA OCZYSZCZONA
+########################################################################
+
