6986bb501fbe94c12594d11508497cf0bc15bc82
Jan Osiecki <s186908@student.pg.edu.pl>
2025-01-13 15:25:36 +0100

    dodałem kod do załadowania pliku i sprawdzenia jego zawartości

---
+++ analiza/Analiza_danych.R
@@ -0,0 +1,13 @@
+# tutaj opracujemy wartości NA
+install.packages("tidyverse")
+install.packages("naniar")
+
+# ładowanie pakietów (środa)
+library(naniar)
+library(tidyverse)
+library(readr)
+
+load("analiza/statystyka_opisowa.Rda")
+
+str(dane3)
+glimpse(dane3)
